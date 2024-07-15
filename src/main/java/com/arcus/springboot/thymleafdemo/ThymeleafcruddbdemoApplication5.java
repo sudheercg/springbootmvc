36e7ce1a069cfc506f5e29ad3c360b2e1d039c4b
sudheercg <sudheer@codegnan.com>
2024-07-15 20:31:17 +0530

    first commit

---
+++ src/main/java/com/arcus/springboot/thymleafdemo/ThymeleafcruddbdemoApplication5.java
@@ -0,0 +1,17 @@
+package com.arcus.springboot.thymleafdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafcruddbdemoApplication5 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafcruddbdemoApplication5.class, args);
+	}
+	
+	
+	/* this is my spring boot starting point */
+
+}
+
